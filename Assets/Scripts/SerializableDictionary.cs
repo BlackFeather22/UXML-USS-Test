b66c737687f0a91f2074e51deb986de28098eeb8
BlackFeather22 <Mojtaba_n.m.m@windowslive.com>
2022-11-16 15:33:48 +0300

    Dictionary
    1- Added a dictionary scripts to test a custom serialized dictionary.
    2- Organized the folders a little bit
    3- Change the version of this project's Editor from 2021.3.0 to 2021.3.4f1

---
+++ Assets/Scripts/SerializableDictionary.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerializableDictionary<TKey,TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+{
+    [SerializeField]
+    private List<TKey> keys = new List<TKey>();
+
+    [SerializeField]
+    private List<TValue> values = new List<TValue>();
+
+    TKey defaultKey;
+    TValue defaultValue;
+
+    public void OnBeforeSerialize()
+    {
+        keys.Clear();
+        values.Clear();
+        foreach (KeyValuePair<TKey,TValue> pair in this)
+        {
+            keys.Add(pair.Key);
+            values.Add(pair.Value);
+        }
+    }
+
+    public void OnAfterDeserialize()
+    {
+        this.Clear();
+
+        while(keys.Count != values.Count)
+        {
+            if(keys.Count > values.Count)
+            {
+                if(keys.FindAll((key)=> Equals(key,defaultKey)).Count > 1)
+                {
+                    Debug.LogError("Please repalce or remove the default key in the dictionary");
+                    keys.RemoveAt(keys.Count - 1);
+                    break;
+                }
+
+                keys[keys.Count - 1] = defaultKey;
+                values.Add(defaultValue);
+            }
+            else if (keys.Count < values.Count)
+                values.RemoveAt(values.Count - 1);
+        }
+
+        for (int i = 0; i < keys.Count; i++)
+        {
+            this.Add(keys[i], values[i]);
+        }
+
+    }
+
+}
+
+[System.Serializable]
+public class Dictionary_GameObject_GameObject: SerializableDictionary<GameObject, GameObject> { }
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] Dictionary_GameObject_GameObject dictionary;
+
+}
